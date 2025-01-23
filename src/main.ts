02dfe43ae646b31ba3d5c0a4fd86984860355a9a
zahiriniamahyar <zahiriniamahyar@gmail.com>
2025-01-23 11:29:24 +0330

    configuration of vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import {createApp} from "vue";
+import {createPinia} from "pinia";
 
-import App from './App.vue'
-import router from './router'
-import {createVuetify} from 'vuetify'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
-const vuetify = createVuetify()
+// Vuetify
+import "vuetify/styles";
+import {createVuetify} from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-app.use(vuetify)
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
-app.mount('#app')
+
+app.use(vuetify);
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
