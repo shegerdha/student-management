81cc70b7f7a4d27c3c7b2607a79014e551f57944
zahiriniamahyar <zahiriniamahyar@gmail.com>
2025-01-26 20:33:59 +0330

    ui fix

--- src/views/dialogs/studentManagement/RemoveDialog.vue
+++ src/views/dialogs/studentManagement/RemoveDialog.vue
@@ -8,22 +8,20 @@ const emit = defineEmits(["handleConfirm", "handleClose"]);
 </script>
 
 <template>
-  <div class="py-4">
-    <v-dialog v-model="isModalOpen" max-width="600">
-      <template v-slot:activator="{ props: activatorProps }">
-        <slot name="activator" v-bind="activatorProps"></slot>
-      </template>
+  <v-dialog v-model="isModalOpen" max-width="600">
+    <template v-slot:activator="{ props: activatorProps }">
+      <slot name="activator" v-bind="activatorProps"></slot>
+    </template>
 
-      <v-card>
-        <v-card-title>حذف دانشجو</v-card-title>
-        <v-card-text> آیا از حذف این دانشجو مطمئن هستید؟</v-card-text>
-        <v-card-actions>
-          <v-btn color="grey" @click="emit('handleClose')">انصراف</v-btn>
-          <v-btn color="red" @click="emit('handleConfirm', compProps.index)"
-            >حذف
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+    <v-card>
+      <v-card-title>حذف دانشجو</v-card-title>
+      <v-card-text> آیا از حذف این دانشجو مطمئن هستید؟</v-card-text>
+      <v-card-actions>
+        <v-btn color="grey" @click="emit('handleClose')">انصراف</v-btn>
+        <v-btn color="red" @click="emit('handleConfirm', compProps.index)"
+          >حذف
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
